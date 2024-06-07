db777dd39fe3cfadaf1200cca4587db05cb78bda
Dmitriy Tonkikh <dimatonkih@gmail.com>
2024-06-07 21:24:01 +0500

    tauri@2-beta
    Signed-off-by: Dmitriy Tonkikh <dimatonkih@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from "vite-plugin-svgr"
+import { internalIpV4 } from "internal-ip";
+
+const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,10 +14,20 @@ export default defineConfig({
   ],
   base: "./",
   clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
+    host: mobile ? "0.0.0.0" : false,
+    hmr: mobile
+        ? {
+          protocol: "ws",
+          host: await internalIpV4(),
+          port: 1421,
+        }
+        : undefined,
     watch: {
+      // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
